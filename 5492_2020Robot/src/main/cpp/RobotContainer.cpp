9b5a352d2a4c9a0e9b4ef5de96331abd918f9f44
ArcaneBow7258 <TempestSTorm7258@gmail.com>
2020-01-07 20:36:31 -0500

    I made it in 2020 VS Code now
    Yeah the first time I made it in 2019

--- 5492_2020Robot/src/main/cpp/RobotContainer.cpp
+++ 5492_2020Robot/src/main/cpp/RobotContainer.cpp
@@ -5,7 +5,20 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "commands/ExampleCommand.h"
+#include "RobotContainer.h"
 
-ExampleCommand::ExampleCommand(ExampleSubsystem* subsystem)
-    : m_subsystem{subsystem} {}
+RobotContainer::RobotContainer() : m_autonomousCommand(&m_subsystem) {
+  // Initialize all of your commands and subsystems here
+
+  // Configure the button bindings
+  ConfigureButtonBindings();
+}
+
+void RobotContainer::ConfigureButtonBindings() {
+  // Configure your button bindings here
+}
+
+frc2::Command* RobotContainer::GetAutonomousCommand() {
+  // An example command will be run in autonomous
+  return &m_autonomousCommand;
+}
