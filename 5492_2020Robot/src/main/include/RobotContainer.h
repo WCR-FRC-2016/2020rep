3592de9e8305439bf7489904ba3f1b4b93ca8331
Joe <newegg@walko.org>
2020-03-18 16:09:23 -0400

    Readability and comments added

--- 5492_2020Robot/src/main/include/RobotContainer.h
+++ 5492_2020Robot/src/main/include/RobotContainer.h
@@ -4,7 +4,9 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+//
+// 2020Mar17 Walko - Added some comment and reordered some statements for better clarity
+//
 #pragma once
 
 #include <frc2/command/Command.h>
@@ -56,17 +58,23 @@ class RobotContainer {
   ShootingAuto m_shootingAuto{&m_driveBase, &m_insideCollector, &m_turret};
   StandShootAuto m_standShootAuto{& m_turret, &m_insideCollector, &m_driveBase};
   TenchAuto m_tenchAuto{&m_driveBase, &m_insideCollector, &m_collector, &m_turret};
-  frc2::InstantCommand m_TrackX{[this] {return m_turret.ISee();} , {&m_turret} };
-  frc2::Button m_manY{[&] {return m_manStick.GetYButton();}};
 
   frc::XboxController m_driverStick{0};
   frc::XboxController m_manStick{1};
+
+  // Track and rotate turret via Limelight - Manipulator Y Button
+  frc2::Button m_manY{[&] {return m_manStick.GetYButton();}};
+  frc2::InstantCommand m_TrackX{[this] {return m_turret.ISee();} , {&m_turret} };
+  
+  // Reverse driving - Driver B Button
   frc2::Button m_driverB{[&] {return m_driverStick.GetBButtonPressed();} };
   frc2::InstantCommand m_reverseDrive{[this] {m_driveBase.reverseDrive(true);} };
+
+  // Slow driving - Driver Y Button
   frc2::Button m_driverY{[&] {return m_driverStick.GetYButtonPressed();} };
   frc2::InstantCommand m_slowDrive{[this] {m_driveBase.slowDrive(true);} };
 
-
+  // Lifting
   frc2::Button m_driverRT{[&] {return (0.5 < m_driverStick.GetTriggerAxis(frc::GenericHID::kRightHand));} };
   frc2::Button m_driverLT{[&] {return (0.5 < m_driverStick.GetTriggerAxis(frc::GenericHID::kLeftHand));} };
   frc2::Button m_driverStart{[&] {return m_driverStick.GetStartButton();} };
@@ -77,18 +85,27 @@ class RobotContainer {
   frc2::InstantCommand m_Latch{[this] {m_doYouEvenLift.Latch();}, {&m_doYouEvenLift} };
   frc2::InstantCommand m_Delatch{[this] {m_doYouEvenLift.Delatch();}, {&m_doYouEvenLift} };
 
-
+  // Collect Balls - Manipulator A Button
   frc2::Button m_manA{[&] {return m_manStick.GetAButton();} };
   frc2::InstantCommand m_collection{[this] {m_insideCollector.Collection();}, {&m_insideCollector} };
+
+  // Spit Balls - Manipulator B Button
   frc2::Button m_manB{[&] {return m_manStick.GetBButton();} };
   frc2::InstantCommand m_spit{[this] {m_insideCollector.Spitting();}, {&m_insideCollector} };
+
+  // Shoot - Manipulator Right Trigger
   frc2::Button m_manRT{[&] {return (0.5 < m_manStick.GetTriggerAxis(frc::GenericHID::kRightHand));} };
   frc2::InstantCommand m_shoot{[this] {m_insideCollector.Shooting(ShootingSpeed);}, {&m_insideCollector} };
+
+  // ShootFast - Manipulator Start Button
   frc2::Button m_manStart{[&]{return m_manStick.GetStartButton();} };
   frc2::InstantCommand m_fastshoot{[this] {m_insideCollector.Shooting(FastShootingSpeed);}, {&m_insideCollector} };
-  frc2::InstantCommand m_halfHood{[this] {m_turret.AutoyAxis(300);}, {&m_turret} };
-  frc2::Button m_manLBumper{[&]{return m_manStick.GetBumper(frc::GenericHID::kLeftHand);} }; 
 
+  // Set Hood - Manupulator Left Bumper
+  frc2::Button m_manLBumper{[&]{return m_manStick.GetBumper(frc::GenericHID::kLeftHand);} }; 
+  frc2::InstantCommand m_halfHood{[this] {m_turret.AutoyAxis(300);}, {&m_turret} };
+  
+  //  Change up/down state of collector - Manipulator X Button
   frc2::Button m_manX{[&] {return m_manStick.GetXButtonPressed();} };
   frc2::InstantCommand m_stateChange{[this] {m_collector.StateChange(); }, {&m_collector} };
 
